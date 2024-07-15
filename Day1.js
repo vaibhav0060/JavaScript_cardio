a29575601b2354a332762018e28f1199df428814
Chaudhari <2138663@cognizant.com>
2024-07-15 23:54:27 +0530

    day1 problem added

---
+++ Day1.js
@@ -0,0 +1,20 @@
+// 1  reverse the Strings 
+function reverseStr(str) {
+    // let revStr= str.split("").reverse().join("")
+    // return revStr ; 
+// let element = "";
+//     for (let i = str.length; i >= 0; i--) {
+//         element = element + str[i];
+//     }
+//     return element
+
+let revStr = "";
+for(let char of str){
+    revStr = char + revStr;
+
+}
+return revStr
+
+}
+console.log(reverseStr("reverse"));
+
